5cbe2aaac278171da7ef5f4363ff7eeb0437872a
Anish Konanki <anishkonanki@gmail.com>
2023-03-30 21:22:41 -0700

    progress

--- browser.py
+++ browser.py
@@ -1,1 +1,68 @@
-# The starter code is a blank file. Have fun :)
+import socket
+import ssl
+import time
+import sys
+
+def request(url):
+    scheme, url = url.split("://", 1)
+    assert scheme in ["http", "https"], \
+        "Unknown scheme {}".format(scheme)
+    
+    host, path = url.split("/", 1)
+    path = "/" + path
+    port = 80 if scheme == "http" else 443
+
+    if ":" in host:
+        host, port = host.split(":", 1)
+        port = int(port)
+
+    s = socket.socket(
+        family=socket.AF_INET,
+        type=socket.SOCK_STREAM,
+        proto=socket.IPPROTO_TCP,
+    )
+    s.connect((host, port))
+
+    if scheme == "https":
+        ctx = ssl.create_default_context()
+        s = ctx.wrap_socket(s, server_hostname=host)
+    
+    s.send("GET {} HTTP/1.0\r\n".format(path).encode("utf8") + 
+       "Host: {}\r\n\r\n".format(host).encode("utf8"))
+    response = s.makefile("r", encoding="utf8", newline="\r\n")
+
+    statusline = response.readline()
+    version, status, explanation = statusline.split(" ", 2)
+    assert status == "200", "{}: {}".format(status, explanation)
+
+    headers = {}
+    while True:
+        line = response.readline()
+        if line == "\r\n":
+            break
+        header, value = line.split(":", 1)
+        headers[header.lower()] = value.strip()
+    
+    assert "transfer-encoding" not in headers
+    assert "content-encoding" not in headers
+
+    body = response.read()
+    s.close()
+    return headers, body
+
+def show(body):
+    tag = False
+    for t in body:
+        if t == "<":
+            tag = True
+        elif t == ">":
+            tag = False
+        elif not tag:
+            print(t, end="")
+
+def load(url):
+    headers, body = request(url)
+    show(body)
+
+if __name__ == "__main__":
+    load(sys.argv[1])
